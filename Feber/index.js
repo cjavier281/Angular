6faa91b2a5c29823d3b10764227a171cd451bd11
Carlos Javier Marentes <cjavier28@hotmail.com>
2022-04-09 11:00:41 -0500

    Se ajusta proyecto a js

--- Feber/index.js
+++ Feber/index.js
@@ -1,11 +1,27 @@
+"use strict";
 console.log("Algo");
-var edades = 23;
-var alturaf = 2.93;
-var estudiof = "segundarios";
-var activof = false;
-var rrecorrer = [6, 7, 8, 9];
+let edades = 23;
+let alturaf = 2.93;
+let estudiof = "segundarios";
+let activof = false;
+let rrecorrer = [6, 7, 8, 9];
 rrecorrer.push(2);
-for (var _i = 0, rrecorrer_1 = rrecorrer; _i < rrecorrer_1.length; _i++) {
-    var numeros = rrecorrer_1[_i];
+for (let numeros of rrecorrer) {
     console.log(numeros);
 }
+var Operacions;
+(function (Operacions) {
+    Operacions[Operacions["Suma"] = 0] = "Suma";
+    Operacions[Operacions["Resta"] = 1] = "Resta";
+    Operacions[Operacions["Multiplicacion"] = 2] = "Multiplicacion";
+    Operacions[Operacions["Division"] = 3] = "Division";
+})(Operacions || (Operacions = {}));
+;
+let feber;
+feber = 3;
+feber = false;
+feber = "blablabla";
+let carlos;
+carlos = 10;
+carlos = true;
+carlos = "ejejejej";
