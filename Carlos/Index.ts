55175f1b16fd53b6e5985e2ee124e98350811373
Carlos Javier Marentes <cjavier28@hotmail.com>
2022-04-08 23:38:39 -0500

    Se  compila js

--- Carlos/Index.ts
+++ Carlos/Index.ts
@@ -2,3 +2,11 @@ let edad:number=23;
 let alturac: number = 1.92;
 let estudiosc:string = 'primarios';
 let activoc: boolean = true;
+
+let  arreglo:number[] =[2,3,4,5];
+arreglo.push(6);
+
+for(let elem of arreglo  ){
+
+    console.log(elem);
+}
---
+++ Carlos/index.js
@@ -0,0 +1,10 @@
+var edad = 23;
+var alturac = 1.92;
+var estudiosc = 'primarios';
+var activoc = true;
+var arreglo = [2, 3, 4, 5];
+arreglo.push(6);
+for (var _i = 0, arreglo_1 = arreglo; _i < arreglo_1.length; _i++) {
+    var elem = arreglo_1[_i];
+    console.log(elem);
+}
