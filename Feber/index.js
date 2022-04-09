db10647cf6231696778e592732ad5d1fc65c6117
feber <afenuar@hotmail.com>
2022-04-08 23:48:40 -0500

    arreglos

---
+++ Feber/index.js
@@ -0,0 +1,11 @@
+console.log("Algo");
+var edades = 23;
+var alturaf = 2.93;
+var estudiof = "segundarios";
+var activof = false;
+var rrecorrer = [6, 7, 8, 9];
+rrecorrer.push(2);
+for (var _i = 0, rrecorrer_1 = rrecorrer; _i < rrecorrer_1.length; _i++) {
+    var numeros = rrecorrer_1[_i];
+    console.log(numeros);
+}
--- Feber/index.ts
+++ Feber/index.ts
@@ -3,3 +3,12 @@ let edades:number=23;
 let alturaf: number = 2.93;
 let estudiof:string = "segundarios"
 let activof: boolean= false
+
+let rrecorrer: number[]=[6,7,8,9];
+rrecorrer.push(2);
+
+for(let numeros of rrecorrer ){
+    console.log(numeros);
+}
+
+enum Operacionf {Suma, Resta, Multiplicacion, Divicion};
